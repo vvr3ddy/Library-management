5a312241545f99879ee0f37a6618ef46f6b687cb
vvr3ddy <vvr-rockstar@bmsit.in>
2020-09-05 17:41:52 +0530

    :gear: Validation Functionality[WIP]
    + Added validation for username
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -7,13 +7,45 @@
           <p class="header">Join our community</p>
           <h1 class="join-us font-weight-bold">Create your account</h1>
           <div class="text-left">
-          <p class="font-weight-bold">Username <span class="text-danger">*</span></p>
-          <b-form-input type="name"></b-form-input>
-          <p class="font-weight-bold">Email address <span class="text-danger">*</span></p>
-          <b-form-input type="email"></b-form-input>
-          <p class="font-weight-bold">Password <span class="text-danger">*</span></p>
-          <b-form-input type="password"></b-form-input>
-          <vue-recaptcha sitekey="6LcFG8gZAAAAAAOMco4cNHuajCd1tBJvvXCxi1cI" :loadRecaptchaScript="true" class="mt-2"></vue-recaptcha>
+
+            <label for="username-input" class="font-weight-bold">Username</label>  
+            <b-form-input
+              id="username-input"
+              v-model="username"
+              :state="usernameState"
+              type="text"
+              aria-describedby="username-feedback"
+              trim>
+            </b-form-input>
+              <b-form-invalid-feedback id="username-feedback">
+                Enter at least 6 letters
+              </b-form-invalid-feedback>
+
+
+            <label for="email-input" class="font-weight-bold">Email address</label>  
+            <b-form-input
+              id="email-input"
+              v-model="email"
+              :state="emailState"
+              type="email"
+              trim></b-form-input>
+              <b-form-invalid-feedback id="email-feedback">
+                Enter a valid email format
+              </b-form-invalid-feedback>
+
+
+          <label for="password-input" class="font-weight-bold">Password</label>  
+            <b-form-input
+              id="password-input"
+              v-model="password"
+              :state="passwordState"
+              type="password"
+              trim></b-form-input>
+              <b-form-invalid-feedback id="password-feedback">
+                Enter at least 8 letters
+              </b-form-invalid-feedback>
+
+          <vue-recaptcha sitekey="Site Key Here" :loadRecaptchaScript="true" class="mt-2"></vue-recaptcha>
           </div>
           <b-button variant="primary" class="signUp w-100" to="#" size="lg">Create account</b-button>
         </b-col>
@@ -35,6 +67,9 @@ import VueRecaptcha from 'vue-recaptcha';
 export default {
 	data () {
 		return {
+			username: '',
+			email: '',
+			password: '',
 			sitekey: '6LcFG8gZAAAAAAOMco4cNHuajCd1tBJvvXCxi1cI',
 			form: {
 				robot: false
@@ -50,6 +85,12 @@ export default {
 		onVerify: function (response) {
 			if (response) this.form.robot = true;
 		},
+	},
+
+	computed: {
+		usernameState () {
+			return this.username.length > 6 ? true : false
+		}
 	},
 	components: {
 		'vue-recaptcha': VueRecaptcha
